4d533054ae3789b938815f9dfd2c889b794052eb
Tomoyuki Kashiro <ksrtmyk@gmail.com>
2014-01-01 11:49:37 +0900

    add shell

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-initialize-project
-==================
+# initialize project
+
+```shell
+  $ prj.sh -t init -d ${DEVICE} -n ${APP_NAMe}
+```
+
+e.g. `prj.sh -t init -d sp -n MyAppSp`
+
+# build app
+
+```shell
+  $ prj.sh -t build -d ${DEVICE}
+```
+
+e.g. `prj.sh -t init -d sp`
+
+# Modify prject config for myself
+
+## bower.json
+
+```json
+  "dependencies": {
+    "jquery": "~1.10.2",
+    "underscore": "*",
+    "GlobalJS": "kashiro/GlobalJS",
+    "js-flipsnap":"pxgrid/js-flipsnap"
+  },
+```
+
+## .jshint.rc
+
+```json
+    "globals": {
+        "_": true,
+        "Global": true,
+        "$": true
+    }
+```
---
+++ prj.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+
+########################################
+## valuable
+########################################
+
+TYPE=""
+DEVICE=""
+APP_NAME=""
+CMDNAME=`basename $0`
+BUILD_DIR='dist'
+
+########################################
+## manage arguments
+########################################
+
+while getopts t:d:n: OPT
+do
+  case $OPT in
+    "t" ) TYPE="$OPTARG" ;;
+    "d" ) DEVICE="$OPTARG" ;;
+    "n" ) APP_NAME="$OPTARG" ;;
+  esac
+done
+
+if [ ${TYPE} = "" -o ${DEVICE} = "" ] ; then
+  echo "Usage: ${CMDNAME} [-t type of operation init|build] [-d device name pc|sp|tb] [-n name of app]" 1>&2
+  exit 1
+fi
+
+if [ ${TYPE} = "init" ] ; then
+  if [ ${DEVICE} = "" -o "${APP_NAME}" = "" ] ; then
+    echo "Usage: ${CMDNAME} -type init [-d device name pc|sp|tb] [-n name of app]" 1>&2
+    exit 1
+  fi
+fi
+
+########################################
+## for initialize project
+########################################
+
+if [ ${TYPE} = 'init' ] ; then
+
+  mkdir ${DEVICE}
+  cd ${DEVICE}
+  yo webapp ${APP_NAME}
+  cd -
+
+  exit 0
+fi
+
+########################################
+## for build project
+########################################
+
+if [ ${TYPE} = 'build' ] ; then
+
+  ## init build directory
+  if [ ! -d ${BUILD_DIR} ]; then
+    mkdir ${BUILD_DIR}
+  fi
+
+  ## execute grunt build
+  cd ${DEVICE}
+  grunt build
+  cd -
+
+  ## copy built app
+  if [ ${DEVICE} = 'pc' ] ; then
+    cp -rf ${DEVICE}/dist/* ${BUILD_DIR}/
+  else
+    cp -rf ${DEVICE}/dist ${BUILD_DIR}/${DEVICE}
+  fi
+
+  ## check git statsu
+  git status -sb
+
+  exit 0
+fi
